d949062f4ddd6bc3b3dad2991f528a4b5ccc6cbd
Diego <msnimpu@gmail.com>
2020-02-13 14:50:16 +0300

    Изменено исключение остствия пар

--- src/main/java/me/ling/kipfin/timetable/exceptions/timetable/NoSubjectsException.java
+++ src/main/java/me/ling/kipfin/timetable/exceptions/timetable/NoSubjectsException.java
@@ -1,13 +1,34 @@
 package me.ling.kipfin.timetable.exceptions.timetable;
 
+import me.ling.kipfin.core.utils.DateUtils;
+
+import java.time.LocalDate;
+
 /**
- * Исключение когда нет предметов врасписании
+ * Исключение когда нет предметов в расписании
  */
 public class NoSubjectsException extends RuntimeException {
-    public NoSubjectsException(String group, String date){
+    private final String state;
+    private final String date;
+
+    public NoSubjectsException(String group, String date) {
         super("Для группы " + group + " на дату " + date + " нет предметов!");
+        this.state = group;
+        this.date = date;
+
     }
-    public NoSubjectsException(String group){
+
+    public NoSubjectsException(String group) {
         super("Для группы " + group + " нет предметов!");
+        this.state = group;
+        this.date = DateUtils.toLocalDateString(LocalDate.now());
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getDate() {
+        return date;
     }
 }
